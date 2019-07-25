8573d9c4e2202c6b971c2af4776d8552bb7a2014
William Chen <seed45699@gmail.com>
2019-07-25 20:40:57 +0800

    project init

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const merge = require("merge-stream");
+
+function modules() {
+    //jQuery
+    var jquery = gulp.src('./node_modules/jquery/dist/jquery.slim.min.js')
+        .pipe(gulp.dest('./stylesheets/js'));
+    //jQuery Easing
+    var easing = gulp.src('node_modules/jquery.easing/jquery.easing.min.js')
+        .pipe(gulp.dest('./stylesheets/js'));
+    //popper.js
+    var popper = gulp.src('./node_modules/popper.js/dist/umd/popper.min.js')
+        .pipe(gulp.dest('./stylesheets/js'));
+    //bootstrap.js
+    var bootstrap = gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js')
+        .pipe(gulp.dest('./stylesheets/js'));
+    return merge(jquery, easing, popper, bootstrap);
+}
+gulp.task('default', modules);
